43fb0045668d4696b9f7effa9e15328e18b13263
Kenneth Petiote <kenneth.petiote@gmail.com>
2021-06-07 11:48:20 -0400

    Modified questions list

--- index.js
+++ index.js
@@ -1,26 +1,109 @@
-// Variables assignment
+// DEPENDENCIES
+// ---------------------------------------------------------------------------
+
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
 
-// Array of questions for user
+// ARRAY OF QUESTIONS
+// ---------------------------------------------------------------------------
+
 const questions = [
+    // Github Username
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the name of your Github username?',
+        validate: (value) => {
+            if (value) { 
+                return true 
+            } else { 
+                return 'Please enter your Github username.' }
+        },
+    },
+    // Email Address
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your preferred email address?',
+        validate: (value) => {
+            if (emailValidator.validate(value)) {
+                return true
+            } else { 
+                return 'Please enter a valid email address' }
+        },
+    },
     // Name of Repository
     {
         type: 'input',
         name: 'repository',
         message: 'What is the name of your repository?',
         validate: (value) => {
-            if (value) { return true } else { return 'Please enter your project title.' }
+            if (value) { 
+                return true 
+            } else { 
+                return 'Please enter your repository name.' }
         },
     },
-    // Name of Repository
+    // Title of Project
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of your repository?',
+        message: 'What is the title of your project?',
         validate: (value) => {
-            if (value) { return true } else { return 'Please enter your project title.' }
+            if (value) { 
+                return true 
+            } else { 
+                return 'Please enter your project title.' }
+        },
+    },
+    // Description of Project
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write the description of your project.',
+        validate: (value) => {
+            if (value) { 
+                return true 
+            } else { 
+                return 'Please enter your project description.' }
+        },
+    },
+    // Project Installation
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter instructions to install the program.',
+        validate: (value) => {
+            if (value) { 
+                return true 
+            } else { 
+                return 'Please enter instructions to install the program.' }
+        },
+    },
+    // Project Usage
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information of this project?',
+        validate: (value) => {
+            if (value) { 
+                return true 
+            } else { 
+                return 'Please enter project usage information.' }
+        },
+    },
+    // Project Licenses
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose the licenses for your project?',
+        choices: ['Apache License 2.0','Boost Software License 1.0','GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'MIT License','Mozilla Public License 2.0'],
+        validate: (value) => {
+            if (value) { 
+                return true 
+            } else { 
+                return 'Please choose a license.' }
         },
     },
 ]
